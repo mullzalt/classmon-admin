cc2053520b4d6548b6952754aeff893bccd5ab78
mullzalt <58712618+mullzalt@users.noreply.github.com>
2019-12-29 22:03:00 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Admin } from 'react-admin';
+import { Resource, ListGuesser } from 'react-admin';
+import simpleRestProvider from 'ra-data-simple-rest';
+import authProvider from './authProvider';
+
+
+
+const dataProvider = simpleRestProvider('http://localhost:5000/');
+const App = () => 
+    <Admin dataProvider={dataProvider}>
+       
+    </Admin>;
+
+export default App;
---
+++ src/myDataProvider.js
@@ -0,0 +1,23 @@
+import { fetchUtils } from 'react-admin';
+import { stringify } from 'query-string';
+
+const apiUrl = 'https://localhost:5000';
+const httpClient = fetchUtils.fetchJson;
+
+export default{
+    getList: (resource, params) => {
+        const { page, perPage } = params.pagination;
+        const { field, order } = params.sort;
+        const query = {
+            sort: JSON.stringify([field, order]),
+            range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
+            filter: JSON.stringify(params.filter),
+        };
+        const url = `${apiUrl}/${resource}?${stringify(query)}`;
+
+        return httpClient(url).then(({ headers, json }) => ({
+            data: json,
+            total: parseInt(headers.get('content-range').split('/').pop(), 10),
+        }));
+    },
+}
