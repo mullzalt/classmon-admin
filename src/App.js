3b3d52a473fe48ed266e0af95051bd58aaa17769
whysetiawan <whysetiawan27@gmail.com>
2019-12-30 23:14:16 +0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Admin } from 'react-admin';
 import { Resource, ListGuesser } from 'react-admin';
 import simpleRestProvider from 'ra-data-simple-rest';
-import authProvider from './authProvider';
+// import authProvider from './authProvider';
 
 
 
 const dataProvider = simpleRestProvider('http://localhost:5000/');
-const App = () => 
+const App = () =>
     <Admin dataProvider={dataProvider}>
-       
+
     </Admin>;
 
 export default App;
